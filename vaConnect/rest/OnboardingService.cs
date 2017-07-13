16ab69918774c7e4885c3a9db49ea7e26eae9da6
Fil <Fil@DESKTOP-P69P4JG>
2017-07-13 13:06:17 +0300

    REST service

--- vaConnect/rest/OnboardingService.cs
+++ vaConnect/rest/OnboardingService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 namespace vaConnect
 {
     class OnboardingService
@@ -21,9 +23,33 @@ namespace vaConnect
             }
             return instance;
         }
-        public void getWiFiProfile(String token, String identifier, WiFiProfile wp)
+        public async void getWiFiProfile(String token, String identifier, WiFiProfile z)
         {
+            Uri baseUri = new Uri("https://vmnac-int.fon.com/onboarding/windows/result/");
+            String add = "?token=" + token + "&identifier=" + identifier;
+            Uri myUri = new Uri(baseUri, add);
+            // Create a request for the URL. 
+            WebRequest request = WebRequest.Create(
+              myUri);
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = await request.GetResponseAsync();
+            // Display the status.
 
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            String responseFromServer = reader.ReadToEnd();
+
+
+            z = JsonConvert.DeserializeObject<WiFiProfile>(responseFromServer);
+
+            // Clean up the streams and the response.
+            //  reader
+            //   response.Close();
         }
     }
 }
