5086712aa4b6c9398046bbdddcf300ba4cb3abc6
Fil <Fil@DESKTOP-P69P4JG>
2017-07-19 21:57:28 +0300

    final test

--- vaConnect/App.xaml.cs
+++ vaConnect/App.xaml.cs
@@ -149,7 +149,7 @@ namespace vaConnect
             if (args.Kind == ActivationKind.Protocol)
             {
 
-                
+
                 ProtocolActivatedEventArgs eventArgs = args as ProtocolActivatedEventArgs;
                 // Handle URI activation
                 // The received URI is eventArgs.Uri.AbsoluteUri
@@ -157,13 +157,15 @@ namespace vaConnect
                 WwwFormUrlDecoder decoder = new WwwFormUrlDecoder(myUri1.Query);
                 String token = decoder.GetFirstValueByName("token");
                 String identifier = decoder.GetFirstValueByName("identifier");
-                int numVal = Int32.Parse("identifier");
+                String Ssid;
+                String password;
+                int numVal = Int32.Parse(identifier);
                 WiFiProfile z = new WiFiProfile();
 
                 z = await OnboardingService.getInstance().getWiFiProfileAsync(token, identifier);
-
+                Ssid = z.getUser_policies().getSsid();
                 parameters.Notification = "Start connecting to " + z.getUser_policies().getEap_type() + " network.";
-                rootFrame.Navigate(typeof(WiFiConfigPage),parameters );
+                rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
                 System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(5));
                 WiFiConfiguration wc = z.getWifiConfiguration();
                 Window.Current.Activate();
@@ -175,7 +177,7 @@ namespace vaConnect
                             InstallOptions.None,
                             "Test");
 
-                
+
                 var access = await WiFiAdapter.RequestAccessAsync();
                 if (access != WiFiAccessStatus.Allowed)
                 {
@@ -186,83 +188,207 @@ namespace vaConnect
                     return;
                 }
                 else
-                { ////////////////////////////////////////////////////////////////////
-
-                    //EAP-TTLS
-                    var result = await Windows.Devices.Enumeration.DeviceInformation.
-                            FindAllAsync(WiFiAdapter.GetDeviceSelector());
-                    if (result.Count >= 1)
-                    {
-                        firstAdapter = await WiFiAdapter.FromIdAsync(result[0].Id);
-                        await firstAdapter.ScanAsync();
-                        var report = firstAdapter.NetworkReport;
-                        //   var message = string.Format("Network Report Timestamp: { 0}", report.Timestamp);
-                        var message = "Find  ";
-                        ResultCollection.Clear();
-                        foreach (var network in report.AvailableNetworks)
+                {
+                    switch (numVal) {
+                        case 2:
+                        {   
+                           ////////////////////////////////////////////////////////////////////
+                       
+                        //EAP-TTLS
+                        var result = await Windows.Devices.Enumeration.DeviceInformation.
+                        FindAllAsync(WiFiAdapter.GetDeviceSelector());
+                        if (result.Count >= 1)
                         {
-                            //Format the network information
-                               message += string.Format("NetworkName: {0}\n", network.Ssid);
-                               ResultCollection.Add(new WiFiNetworkDisplay(network, firstAdapter));
+                            firstAdapter = await WiFiAdapter.FromIdAsync(result[0].Id);
+                            await firstAdapter.ScanAsync();
+                            var report = firstAdapter.NetworkReport;
+                            //   var message = string.Format("Network Report Timestamp: { 0}", report.Timestamp);
+                            var message = "Find  ";
+                            ResultCollection.Clear();
+                            foreach (var network in report.AvailableNetworks)
+                            {
+                                //Format the network information
+                                message += string.Format("NetworkName: {0}\n", network.Ssid);
+                                ResultCollection.Add(new WiFiNetworkDisplay(network, firstAdapter));
 
-                        }
+                            }
+                            var q = ResultCollection.Where(X => X.Ssid == Ssid).FirstOrDefault();
+                            if(q == null)
+                                    {
+                                        parameters.Clear();
+                                        parameters.Notification = "Cannot find network with ssid: " + Ssid;
+                                        parameters.Result = "Net error!";
+                                        rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+                                        return;
+                                    }     
+                            parameters.Clear();
+                            parameters.Notification = message;
+                            rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
 
-                        parameters.Clear();
-                        parameters.Notification = message;
-                        rootFrame.Navigate(typeof(WiFiConfigPage),parameters );
-
-                        WiFiNetworkDisplay selectedNetwork = ResultCollection[0];
+                            WiFiNetworkDisplay selectedNetwork = q;
 
 
-                        WiFiConnectionResult result0;
-                        WiFiReconnectionKind reconnectionKind = WiFiReconnectionKind.Manual;
-                        if (selectedNetwork.AvailableNetwork.SecuritySettings.NetworkAuthenticationType == Windows.Networking.Connectivity.NetworkAuthenticationType.Open80211)
-                        {
-                            result0 = await firstAdapter.ConnectAsync(selectedNetwork.AvailableNetwork, reconnectionKind);
+                            WiFiConnectionResult result0;
+                            WiFiReconnectionKind reconnectionKind = WiFiReconnectionKind.Manual;
+                            if (selectedNetwork.AvailableNetwork.SecuritySettings.NetworkAuthenticationType == Windows.Networking.Connectivity.NetworkAuthenticationType.Open80211)
+                            {
+                                result0 = await firstAdapter.ConnectAsync(selectedNetwork.AvailableNetwork, reconnectionKind);
+                            }
+                            else
+                            {
+                                // Only the password potion of the credential need to be supplied
+                                var credential = new PasswordCredential();
+                                        String Password = z.getUser_policies().getPassword();
+                                credential.Password = Password;
+
+                                result0 = await firstAdapter.ConnectAsync(selectedNetwork.AvailableNetwork, reconnectionKind, credential);
+                            }
+
+                            if (result0.ConnectionStatus == WiFiConnectionStatus.Success)
+                            {
+                                parameters.Clear();
+                                parameters.Notification = string.Format("Successfully connected to {0}.", selectedNetwork.Ssid);
+                                parameters.Result = "Success!";
+                                rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+
+                                // refresh the webpage
+                                // webViewGrid.Visibility = Visibility.Visible;
+                                //  toggleBrowserButton.Content = "Hide Browser Control";
+                                //  refreshBrowserButton.Visibility = Visibility.Visible;
+                            }
+                            else
+                            {
+                                parameters.Clear();
+                                parameters.Result = "Error!";
+                                parameters.Notification = string.Format("Could not connect to {0}. Error: {1}", selectedNetwork.Ssid, result0.ConnectionStatus);
+                                rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+                            }
+
                         }
                         else
                         {
-                            // Only the password potion of the credential need to be supplied
-                            var credential = new PasswordCredential();
-                            String Password = "passIimpf2soppistmmrtw";
-                            credential.Password = Password;
-
-                            result0 = await firstAdapter.ConnectAsync(selectedNetwork.AvailableNetwork, reconnectionKind, credential);
-                        }
-
-                        if (result0.ConnectionStatus == WiFiConnectionStatus.Success)
-                        {
                             parameters.Clear();
-                            parameters.Notification = string.Format("Successfully connected to {0}.", selectedNetwork.Ssid);
-                            parameters.Result = "Success!";
-                            rootFrame.Navigate(typeof(WiFiConfigPage),parameters);
-
-                            // refresh the webpage
-                           // webViewGrid.Visibility = Visibility.Visible;
-                          //  toggleBrowserButton.Content = "Hide Browser Control";
-                          //  refreshBrowserButton.Visibility = Visibility.Visible;
+                            parameters.Notification = "No WiFi Adapter. Or access is denied!";
+                            parameters.Result = "System error!";
+                            rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+                            return;
                         }
-                        else
-                        {
-                            parameters.Clear();
-                            parameters.Result = "Error!";
-                            parameters.Notification = string.Format("Could not connect to {0}. Error: {1}", selectedNetwork.Ssid, result0.ConnectionStatus);
-                            rootFrame.Navigate(typeof(WiFiConfigPage),parameters);
+                        break;
                         }
+                        ///////////////////////////
+                        case 1:
+                            {
+                                parameters.Clear();
+                                parameters.Notification = "Not implemented yet";
+                                parameters.Result = "System waninbg!";
+                                rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
 
-                    }
-                    else
-                    {
-                        parameters.Clear();
-                        parameters.Notification = "No WiFi Adapter. Or access is denied!";
-                        parameters.Result = "System error!";
-                        rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
-                        return;
-                    }
+                                break;
+                            }
+                        case 3:
+                            {
+                                parameters.Clear();
+                                parameters.Notification = "Not implemented yet";
+                                parameters.Result = "System waninbg!";
+                                rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+                               
+                                break;
+                            }
+                        case 4:
+                            {
+                                ////////////////////////////////////////////////////////////////////
+
+                                //EAP-PEAP
+                                var result = await Windows.Devices.Enumeration.DeviceInformation.
+                                FindAllAsync(WiFiAdapter.GetDeviceSelector());
+                                if (result.Count >= 1)
+                                {
+                                    firstAdapter = await WiFiAdapter.FromIdAsync(result[0].Id);
+                                    await firstAdapter.ScanAsync();
+                                    var report = firstAdapter.NetworkReport;
+                                    //   var message = string.Format("Network Report Timestamp: { 0}", report.Timestamp);
+                                    var message = "Find  ";
+                                    ResultCollection.Clear();
+                                    foreach (var network in report.AvailableNetworks)
+                                    {
+                                        //Format the network information
+                                        message += string.Format("NetworkName: {0}\n", network.Ssid);
+                                        ResultCollection.Add(new WiFiNetworkDisplay(network, firstAdapter));
+
+                                    }
+                                    var q = ResultCollection.Where(X => X.Ssid == Ssid).FirstOrDefault();
+                                    if (q == null)
+                                    {
+                                        parameters.Clear();
+                                        parameters.Notification = "Cannot find network with ssid: " + Ssid;
+                                        parameters.Result = "Net error!";
+                                        rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+                                        return;
+                                    }
+                                    parameters.Clear();
+                                    parameters.Notification = message;
+                                    rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+
+                                    WiFiNetworkDisplay selectedNetwork = q;
+
+
+                                    WiFiConnectionResult result0;
+                                    WiFiReconnectionKind reconnectionKind = WiFiReconnectionKind.Manual;
+                                    if (selectedNetwork.AvailableNetwork.SecuritySettings.NetworkAuthenticationType == Windows.Networking.Connectivity.NetworkAuthenticationType.Open80211)
+                                    {
+                                        result0 = await firstAdapter.ConnectAsync(selectedNetwork.AvailableNetwork, reconnectionKind);
+                                    }
+                                    else
+                                    {
+                                        // Only the password potion of the credential need to be supplied
+                                        var credential = new PasswordCredential();
+                                        String Password = z.getUser_policies().getPassword();
+                                        credential.Password = Password;
+
+                                        result0 = await firstAdapter.ConnectAsync(selectedNetwork.AvailableNetwork, reconnectionKind, credential);
+                                    }
+
+                                    if (result0.ConnectionStatus == WiFiConnectionStatus.Success)
+                                    {
+                                        parameters.Clear();
+                                        parameters.Notification = string.Format("Successfully connected to {0}.", selectedNetwork.Ssid);
+                                        parameters.Result = "Success!";
+                                        rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+
+                                        // refresh the webpage
+                                        // webViewGrid.Visibility = Visibility.Visible;
+                                        //  toggleBrowserButton.Content = "Hide Browser Control";
+                                        //  refreshBrowserButton.Visibility = Visibility.Visible;
+                                    }
+                                    else
+                                    {
+                                        parameters.Clear();
+                                        parameters.Result = "Error!";
+                                        parameters.Notification = string.Format("Could not connect to {0}. Error: {1}", selectedNetwork.Ssid, result0.ConnectionStatus);
+                                        rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+                                    }
+
+                                }
+                                else
+                                {
+                                    parameters.Clear();
+                                    parameters.Notification = "No WiFi Adapter. Or access is denied!";
+                                    parameters.Result = "System error!";
+                                    rootFrame.Navigate(typeof(WiFiConfigPage), parameters);
+                                    return;
+                                }
+                                break;
+                            }
+                            ///////////////////////////
+                          
+                    default:
+                            {
+                               break ;
+                            }
+                    }//switch end
                 }
-                //////////////////////////////
-            }             
-
+            }
+          
                 
 
                 
